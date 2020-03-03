226fec4ccdfbd7afeb4f41d15578e5dcf7300a36
jiachenxu <943522184@qq.com>
2020-03-03 18:14:47 +0800

    add

---
+++ src/main/java/com/util/utils/DownLoadUtil.java
@@ -0,0 +1,14 @@
+package com.util.utils;
+
+/**
+ * @Version 1.0
+ * @ClassName DownLoadUtil
+ * @Author xujiachen
+ * @Date 2020/3/3 18:11
+ * @Description TODO
+ */
+public class DownLoadUtil {
+
+
+
+}
