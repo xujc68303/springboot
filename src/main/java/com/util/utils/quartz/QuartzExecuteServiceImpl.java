febc2efbd80348ef3bf2cf979f9076dec22bbcfb
jiachenxu <943522184@qq.com>
2020-05-23 22:23:05 +0800

    add quartz

--- src/main/java/com/util/utils/quartz/QuartzExecuteServiceImpl.java
+++ src/main/java/com/util/utils/quartz/QuartzExecuteServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
