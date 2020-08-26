56da931ba00b5554e872c61f7b57e678450d0b0b
jiachenxu <943522184@qq.com>
2020-08-26 23:23:45 +0800

    add docker

---
+++ docker/src/main/java/com/xjc/docker/DockerApi.java
@@ -0,0 +1,20 @@
+package com.xjc.docker;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Version 1.0
+ * @ClassName DockerApi
+ * @Author jiachenXu
+ * @Date 2020/8/23 22:26
+ * @Description
+ */
+@Configuration
+@Slf4j
+public class DockerApi {
+
+    public void request(){
+        log.warn("hello docker");
+    }
+}
