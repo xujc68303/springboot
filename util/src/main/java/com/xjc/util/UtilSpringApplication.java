25e312fee27602c1228eb240628cc67de72a785d
jiachenxu <943522184@qq.com>
2020-10-15 22:49:06 +0800

    结构重构

---
+++ util/src/main/java/com/xjc/util/UtilSpringApplication.java
@@ -0,0 +1,20 @@
+package com.xjc.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Version 1.0
+ * @ClassName UtilSpringApplication
+ * @Author jiachenXu
+ * @Date 2020/10/15
+ * @Description
+ */
+@SpringBootApplication
+public class UtilSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilSpringApplication.class,args);
+    }
+
+}
