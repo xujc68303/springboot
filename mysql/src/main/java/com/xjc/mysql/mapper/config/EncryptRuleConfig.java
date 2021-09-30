4a6123f250cce6996c1df85a30c8154490bc32e4
xujiachen <xjc@dataqin.com>
2021-09-30 17:25:17 +0800

    update 数据脱敏

--- mysql/src/main/java/com/xjc/mysql/mapper/config/EncryptRuleConfig.java
+++ mysql/src/main/java/com/xjc/mysql/mapper/config/EncryptRuleConfig.java
@@ -1,31 +1,35 @@
 package com.xjc.mysql.mapper.config;
 
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shardingsphere.encrypt.api.EncryptColumnRuleConfiguration;
 import org.apache.shardingsphere.encrypt.api.EncryptRuleConfiguration;
 import org.apache.shardingsphere.encrypt.api.EncryptTableRuleConfiguration;
 import org.apache.shardingsphere.encrypt.api.EncryptorRuleConfiguration;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 /**
- * @Description EncryptRuleConfig
+ * @Description 数据脱敏
  * @Date 2021/7/23 17:29
  * @Created by Xujc
  */
-@Configuration
 public class EncryptRuleConfig {
 
     @Value("${spring.shardingsphere.encrypt.encryptors.encrytor_aes.type}")
     private String encryption;
+    @Value("${spring.shardingsphere.encrypt.encryptors.encrytor_aes.key}")
+    private String keys;
 
     public EncryptRuleConfiguration getEncryptRuleConfiguration(String key, Map<String, String> columnMap, String table) {
         EncryptRuleConfiguration encryptRuleConfig = new EncryptRuleConfiguration();
-
         Properties props = new Properties();
+        if (StringUtils.isBlank(key)) {
+            key = keys;
+        }
         props.setProperty("spring.shardingsphere.encrypt.encryptors.encrytor_aes.key", key);
         EncryptorRuleConfiguration encryptorRuleConfiguration = new EncryptorRuleConfiguration(encryption, props);
 
@@ -37,6 +41,14 @@ public class EncryptRuleConfig {
 
         encryptRuleConfig.getTables().put(table, new EncryptTableRuleConfiguration(columnConfigMaps));
         return encryptRuleConfig;
+    }
+
+    public static void main(String[] args) {
+        EncryptRuleConfig ruleConfig = new EncryptRuleConfig();
+        Map<String, String> columnMap = Maps.newHashMap();
+        columnMap.put("address", "");
+        columnMap.put("password", "");
+        ruleConfig.getEncryptRuleConfiguration("", columnMap, "t_customer");
     }
 
 
